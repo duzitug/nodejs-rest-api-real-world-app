8a2999d604da95aa1f7bc506494003153a02c478
merciof <merciofilho@gmail.com>
2022-11-14 17:58:00 -0300

    create user model and associations

---
+++ models/user.model.js
@@ -0,0 +1,48 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate({ Comment, Article }) {
+      // Comments
+      this.hasMany(Comment, { foreignKey: "user_id" });
+
+      // Articles
+      this.hasMany(Article, { foreignKey: "user_id", onDelete: "CASCADE" });
+
+      // Favorites
+      this.belongsToMany(Article, {
+        through: "user_article",
+        as: "favorites",
+        foreignKey: "user_id",
+      });
+
+      // Followers
+      this.belongsToMany(User, {
+        through: "user_user",
+        as: "followers",
+        foreignKey: "user_id",
+      });
+      this.belongsToMany(User, {
+        through: "user_user",
+        as: "following",
+        foreignKey: "follower_id",
+      });
+    }
+  }
+  User.init(
+    {
+      email: DataTypes.STRING,
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      bio: DataTypes.TEXT,
+      image: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "User",
+      underscored: true,
+      tableName: "users",
+    }
+  );
+  return User;
+};
