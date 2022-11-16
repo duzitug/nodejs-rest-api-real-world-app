70f749a30bb29075c331caa4e942751cd679860d
merciof <merciofilho@gmail.com>
2022-11-16 15:54:45 -0300

    add new route to articles router

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { createArticle } = require("../controller/Articles.controller.js");
+const {
+  createArticle,
+  allArticles,
+} = require("../controller/Articles.controller.js");
 const verifyToken = require("../middleware/verifyToken.js");
 
 // createArticle
 router.post("/create", verifyToken, createArticle);
+
+//? All Articles - by Author/by Tag/Favorited by user
+router.get("/", verifyToken, allArticles);
 
 module.exports = router;
