8fbcf7170ecb84e0ed86dedfa32297848ed7fd22
merciof <merciofilho@gmail.com>
2022-11-16 20:40:26 -0300

    add routes de articles router

--- routes/articles.js
+++ routes/articles.js
@@ -4,13 +4,25 @@ const router = express.Router();
 const {
   createArticle,
   allArticles,
+  singleArticleBySlug,
+  updateBySlug,
+  deleteBySlug,
 } = require("../controller/Articles.controller.js");
 const verifyToken = require("../middleware/verifyToken.js");
 
-// createArticle
+// Create Article
 router.post("/create", verifyToken, createArticle);
 
 //? All Articles - by Author/by Tag/Favorited by user
 router.get("/", verifyToken, allArticles);
+
+// Get Single Article by Slug
+router.get("/:slug", verifyToken, singleArticleBySlug);
+
+// Update Article
+router.put("/:slug", verifyToken, updateBySlug);
+
+// Delete Article
+router.delete("/:slug", verifyToken, deleteBySlug);
 
 module.exports = router;
