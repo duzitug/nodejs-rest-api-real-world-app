455b0ffab3e3bab5a024365a5650bf31ce1b13ef
merciof <merciofilho@gmail.com>
2022-11-21 11:25:26 -0300

    refactor article route

--- route/article.route.js
+++ route/article.route.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const favoriteRoutes = require("../route/favorite.route.js");
+const commentRoutes = require("../route/comment.route.js");
+
 const {
   createArticle,
   allArticles,
@@ -13,7 +16,7 @@ const verifyToken = require("../middleware/verifyToken.js");
 // Create Article
 router.post("/", verifyToken, createArticle);
 
-//? All Articles - by Author/by Tag/Favorited by user
+// All Articles - by Author/by Tag/Favorited by user
 router.get("/", verifyToken, allArticles);
 
 // Get Single Article by Slug
@@ -24,5 +27,11 @@ router.put("/:slug", verifyToken, updateBySlug);
 
 // Delete Article
 router.delete("/:slug", verifyToken, deleteBySlug);
+
+// Favorites routes
+router.use("/", favoriteRoutes);
+
+//Comments Routes
+router.use("/", commentRoutes);
 
 module.exports = router;
