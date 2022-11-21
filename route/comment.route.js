e50051b6a938c070e5da76bfbfb3c39aaf00cf90
merciof <merciofilho@gmail.com>
2022-11-21 11:25:16 -0300

    refactor comment route

--- route/comment.route.js
+++ route/comment.route.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const {
   createComment,
   allComments,
+  deleteComment,
 } = require("../controller/Comment.controller.js");
 const verifyToken = require("../middleware/verifyToken.js");
 
 // Create Comment
-router.post("/:slug", verifyToken, createComment);
+router.post("/:slug/comments", verifyToken, createComment);
 
 // Get All Comments
-router.get("/:slug", allComments);
+router.get("/:slug/comments", verifyToken, allComments);
+
+// Delete Comment
+router.delete("/:slug/comments/:commentId", verifyToken, deleteComment);
 
 module.exports = router;
